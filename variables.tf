498ed3ad468b3a1e5b78edfb07ec50a97dd00d78
GRV <40017100+AwesomeGRV@users.noreply.github.com>
2022-06-29 10:22:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
   default     = "us-east-1"
-  description = "Region where the resources will be deployed."
+  description = "Region where the resources."
 }
