0b140e6b779bd0f982a333308e03b30e358cb1d8
awesomegrv <gauravmishra@outlook.com>
2022-07-06 17:02:17 +0530

    updates

---
+++ trans-gw/transit-gw.tf
@@ -0,0 +1,36 @@
+module "tgw" {
+  source  = "terraform-aws-modules/transit-gateway/aws"
+  version = "~> 2.0"
+
+  name        = "my-tgw"
+  description = "My TGW shared with several other AWS accounts"
+
+  enable_auto_accept_shared_attachments = true
+
+  vpc_attachments = {
+    vpc = {
+      vpc_id       = module.vpc-dev.id
+      subnet_ids   = module.vpc-dev.private_subnets
+      dns_support  = true
+      ipv6_support = true
+
+      tgw_routes = [
+        {
+          destination_cidr_block = "30.0.0.0/16"
+        },
+        {
+          blackhole = true
+          destination_cidr_block = "40.0.0.0/20"
+        }
+      ]
+    }
+  }
+
+  ram_allow_external_principals = true
+  ram_principals = [307990089504]
+
+  tags = {
+    Purpose = "tgw-complete-example"
+  }
+}
+
